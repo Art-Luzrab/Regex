0d4027f4c1384dd86306abad28f1b8040b403f6a
Arthur <arthur91021@gmail.com>
2024-10-11 00:31:32 -0700

    styling

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 let txt = "Programming courses always starts with a hello world example.";
 
-let regex1 = new RegExp("hello");
-let regex2 = /worlds/;
+let regex1 = /hello/;
+let regex2 = /world/;
 
 console.log(regex1.test(txt)); // true
-console.log(regex2.test(txt)); // false
+console.log(regex2.test(txt)); // true
