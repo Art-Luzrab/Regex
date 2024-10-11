18dbd9f761c22c24bd2297211f3eb797b2081fcd
Arthur <arthur91021@gmail.com>
2024-10-11 00:21:19 -0700

    created first regex and tested it

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 "use strict";
 
-let text = "Programming courses always starts with a hello world example.";
+let txt = "Programming courses always starts with a hello world example.";
 
 let regex1 = new RegExp("hello");
-let regex2 = /world/;
+let regex2 = /worlds/;
+
+console.log(regex1.test(txt)); // true
+console.log(regex2.test(txt)); // false
