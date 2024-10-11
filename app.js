3b6eaf97f017048dd718f18477ee1ea17a82234c
Arthur <arthur91021@gmail.com>
2024-10-11 00:57:41 -0700

    regex flags

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 "use strict";
 
-let txt = "Programming courses always starts with a hello world example.";
+let txt = "Programming courses alwayS starts with a hello world example.";
 
 let regex1 = /hello/;
 let regex2 = /world/;
 let regex3 = /\s/; // looking for spaces
+let regex4 = /s\s/gi; // looking for an s followed by a space globally, case insensitive
 
 console.log(regex1.test(txt)); // true
 console.log(regex2.test(txt)); // true
@@ -35,3 +36,20 @@ console.log(txt.split(regex1)); // ['Programming courses always starts with a ',
 // .split() splits a string into an array. The division is based on the regual expression pattern.
 
 console.log(txt.split(regex3)); // (9) ['Programming', 'courses', 'always', 'starts', 'with', 'a', 'hello', 'world', 'example.']
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+/** Regular Expression Flags */
+
+// /pattern/*flags here*; or new RegExp("pattern", "flags");
+
+// g - global, match more than one occurance
+// i - case insensitive match, case doesn't matter
+// m - multi-line match
+
+console.log(txt.match(regex4)); //  ['s ', 'S ', 'S ']
+
+console.log(regex4.exec(txt)); // ['s ', index: 18, input: 'Programming courses alwayS starts with a hello world example.', groups: undefined]
+console.log(regex4.exec(txt)); // ['S ', index: 25, input: 'Programming courses alwayS starts with a hello world example.', groups: undefined]
+console.log(regex4.exec(txt)); // ['s ', index: 32, input: 'Programming courses alwayS starts with a hello world example.', groups: undefined]
+console.log(regex4.exec(txt)); // null
