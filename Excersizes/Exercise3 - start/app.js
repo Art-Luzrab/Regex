775f6692da861197389cf38354b4ba216b068e5a
Arthur <arthur91021@gmail.com>
2024-10-29 14:59:26 -0700

    finished excersize 3

--- Excersizes/Exercise3 - start/app.js
+++ Excersizes/Exercise3 - start/app.js
@@ -6,3 +6,15 @@ Use several different phone numbers to test.
 HINT: You can use the keyup event to respond to entered text. There is a CSS Class for red and green.
 */
 
+const regex = /\(?\d{3}\)?-?\.?\d{3}-?\.?\d{4}/g;
+
+const input = document.getElementById("phone");
+
+input.addEventListener("input", function () {
+  if (regex.test(input.value) === true) {
+    input.classList = "green";
+  } else {
+    input.classList = "red";
+  }
+  console.log(input.value);
+});
